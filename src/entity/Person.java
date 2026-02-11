fd26c23156edc22d1d15251ee762db889988d1d9
FiFiFragolina <ferielboumaya@gmail.com>
2026-02-11 13:09:57 +0100

    Add encapsulation

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,23 +1,26 @@
 package entity;
 
 public class Person {
-   String myname;
-   int myage;
+    private String myname;
+    int myage;
 
     public Person(String myname, int myage) {
         this.myname = myname;
         this.myage = myage;
     }
+    public String getMyname(){
+        return myname;
+    }
 
-    /*public Person(String name, int age){
-           myage = age;
-           myname = name;
-       }*/
-    /*public Person(){
+    public void setMyname(String newMyname){
+        this.myname= newMyname;
+    }
 
-    }*/
+
     public static void main(String[] args) {
        Person myprofile= new Person("firial", 23);
+
+       myprofile.setMyname("Firial11");
         System.out.println("My age is "+myprofile.myage+" My name is "+myprofile.myname);
     }
     }
