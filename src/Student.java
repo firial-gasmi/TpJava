e58d8251744e8b6aee5645dafb5c743f89a5d0f4
FiFiFragolina <ferielboumaya@gmail.com>
2026-02-11 10:04:54 +0100

    Add constructor

--- src/Student.java
+++
@@ -1,6 +0,0 @@
-public class Student {
-// hedha l commit theni
-    public static void main(String[] args){
-
-    }
-}
--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,31 +1,23 @@
 package entity;
 
 public class Person {
-    static String lastname ="Gasmi ";
-    static String firstname ="Firial ";
-    static int age = 23;
-    public int niveau = 123;
-    public static void main(String[] args){
-        System.out.println(firstname+lastname+age);
-        Person ali1 = new Person();
-        Person person2 = new Person();
-        ali1.firstname = "ali ben ali";
-        ali1.lastname = "trabelsi";
-        ali1.age = 24;
+   String myname;
+   int myage;
 
-        /*
-        person2.lastname = "farah";
-        person2.firstname = "Gasmi";
-        person2.age = 27;
-        System.out.println(person1.firstname+person1.lastname+person1.age);
-        System.out.println("First name is "+person2.firstname+"Last name is "+person2.lastname+person2.age);
-
-        entity.Person myObj1 = new entity.Person();
-        entity.Person myObj2 = new entity.Person();
-        System.out.println(myObj1);
-        System.out.println(myObj2);
-
-         */
+    public Person(String myname, int myage) {
+        this.myname = myname;
+        this.myage = myage;
     }
 
-}
+    /*public Person(String name, int age){
+           myage = age;
+           myname = name;
+       }*/
+    /*public Person(){
+
+    }*/
+    public static void main(String[] args) {
+       Person myprofile= new Person("firial", 23);
+        System.out.println("My age is "+myprofile.myage+" My name is "+myprofile.myname);
+    }
+    }
